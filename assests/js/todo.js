859968295b6d3f3295f08977fcaaed4d62165382
chandu <chandrasr.palle@gmail.com>
2018-06-06 16:22:26 +0200

    creating todos

--- assests/js/todo.js
+++ assests/js/todo.js
@@ -1,11 +1,21 @@
 // alert("welcome");
-
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
-$("span").click(function(){
+// $("li").click(function(){
+
+// });
+$("ul").on("click","span", (function(){
     $(this).parent().fadeOut(500,function(){
         $(this).remove();
     });
     event.stopPropagation();
+}));
+$("input[type='text']").keypress(function(event){
+    if(event.which == 13){
+          let todoText = this.value;
+          this.value = ""; 
+          $("ul").append("<li><span>X</span> "+todoText+"</li>");
+    }
+
 });
